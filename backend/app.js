b8532c4b4f334ac539ce51e7825c4634f44b936a
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2024-09-06 23:41:07 +0530

    small correction on PORT code

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use('/api', authRoutes);
 app.use('/api', postRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
