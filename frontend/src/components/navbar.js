6902586899f8db23614ffff667b537a20da77ad1
G Madhu Sudan Reddy <madhureddy2019cse@gmail.com>
2024-09-09 09:54:55 +0530

    initial changes to the Navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,25 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom'
+import React from "react";
+import { Link, useLocation } from "react-router-dom/cjs/react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation(); // Get the current route to highlight active link
+
   return (
     <React.Fragment>
-        <nav className="bg-gray-800 px-20 py-4">
-            <div className="container mx-auto flex items-center justify-between">
-                {/* Logo */}
-                <Link to="/" className="text-white text-xl font-bold">Apex Archives</Link>
+      <nav className="bg-gradient-to-r from-purple-600 to-blue-500 shadow-lg px-20 py-4">
+        <div className="container mx-auto flex items-center justify-between">
+          {/* Logo */}
+          <Link
+            to="/"
+            className="text-white text-2xl font-bold tracking-wide hover:text-gray-300 transition duration-300"
+          >
+            Apex Archives
+          </Link>
 
-                {/* Navigation Links */}
-                <div className="space-x-4">
-                    <Link to="/" className="text-white">Home</Link>
-                    <Link to="/upload" className="text-white">Upload Blog</Link>
-                    <Link to="/about" className="text-white">About</Link>
-                </div>
-            </div>
-        </nav>
+          {/* Navigation Links */}
+          <div className="space-x-8">
+            <Link
+              to="/"
+              className={`text-white hover:text-gray-300 transition duration-300 ${
+                location.pathname === "/" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              Home
+            </Link>
+            <Link
+              to="/upload"
+              className={`text-white hover:text-gray-300 transition duration-300 ${
+                location.pathname === "/upload" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              Upload Blog
+            </Link>
+            <Link
+              to="/about"
+              className={`text-white hover:text-gray-300 transition duration-300 ${
+                location.pathname === "/about" ? "border-b-2 border-white" : ""
+              }`}
+            >
+              About
+            </Link>
+          </div>
+        </div>
+      </nav>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
 
